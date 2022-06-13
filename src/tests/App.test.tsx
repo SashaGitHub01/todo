61d19bbb917a215af6fa515686df9d52ab52fc25
SashaGitHub01 <sashab4b1n@yandex.ru>
2022-06-13 11:03:45 +0300

    test1

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { render, screen, fireEvent, getByTestId } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import App from '../App'
+import userEvent from '@testing-library/user-event'
 
 test('render h1 in app', async () => {
    render(<App />)
@@ -13,8 +14,7 @@ test('create new todo', async () => {
    const form = screen.getByTestId('todo-form')
    const input = screen.getByTestId('todo-input')
    expect(input).toBeInTheDocument()
-
-   fireEvent.change(input, { target: { value: 'create test todo' } })
+   userEvent.type(input, 'create test todo')
    fireEvent.submit(form)
    const todo = screen.getByText(/create test todo/i)
    expect(todo).toBeInTheDocument()
@@ -25,13 +25,13 @@ test('delete todo', async () => {
    const form = screen.getByTestId('todo-form')
    const input = screen.getByTestId('todo-input')
 
-   fireEvent.change(input, { target: { value: 'create test todo' } })
+   userEvent.type(input, 'create test todo')
    fireEvent.submit(form)
    const todo = screen.getByText(/create test todo/i)
    expect(todo).toBeInTheDocument()
 
    const btn = screen.getByTestId('deleteBtn')
-   fireEvent.click(btn)
+   userEvent.click(btn)
 
    const afterDel = screen.queryByText(/create test todo/i)
    expect(afterDel).toBeNull()
